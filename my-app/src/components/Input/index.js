39dc2868a95feac6876921172793d8ad38555f0b
Ben Hensor <benhensor@gmail.com>
2023-04-14 10:26:59 +0100

    folder structure created. input file skeleton function set up

---
+++ my-app/src/components/Input/index.js
@@ -0,0 +1,12 @@
+// importing any neccessary functions and components
+// create the component function
+// export function
+import AddToDoButton from "./Button";
+
+
+export default function ToDoInputTextField() {
+  return <input className="textInput" type="text" placeholder="Enter a ToDo"></input>;
+
+}
+
+// onChange={props.onChange} value={props.value}
---
+++ my-app/src/components/plan.js
@@ -0,0 +1,26 @@
+// Use component tree in README.md to determine states and props
+
+// ### Component Tree
+
+// - App                      - Create App folder ✅
+//   - Input                  - Create Input folder ✅
+//     - text input           - Create Text Folder ✅
+//     - add to list button   - Create Button folder ✅
+//   - List                   - Create List folder ✅
+//     - List Item            - Create List Item folder ✅
+//       - item text          - Create Item Text folder ✅
+//       - delete button      - Put in Button folder ✅
+//     - List Item
+//       - item text
+//       - delete button
+//     - List Item
+//       - item text
+//       - delete button
+//     - List Item
+//       - item text
+//       - delete button
+//         ...
+
+// App Component
+// - Import relevant components
+
