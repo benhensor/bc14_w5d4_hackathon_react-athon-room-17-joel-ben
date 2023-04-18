2db0dd51fed6aa2f0a5eb220bd3b9ad50add3060
Ben Hensor <benhensor@gmail.com>
2023-04-18 18:15:27 +0100

    refactored code to move state handling to the appcomponent

--- my-app/src/components/App/App.js
+++ my-app/src/components/App/App.js
@@ -1,46 +1,44 @@
-// Import React library, styles and app components
 import React from 'react';
 import { useState } from 'react';
 import './App.css';
 import ToDoInput from '../Input';
 import ToDoList from '../List';
 
-// Primary app function
 function App() {
 
-  // State declaration for updating the todo list
-  // Const called 'toDoList', the function to update it's state 'setToDoList', assigned to an empty array in 'useState'
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  }
+
+  console.log(inputValue);
+
+  const handleAddClick = () => {
+    handleAdd(inputValue);
+    setInputValue(''); 
+  }
+
   const [toDoList, setToDoList] = useState([]);
 
-  // Function that takes a 'newTodo' parameter
   const handleAdd = (newToDo) => {
-    // If 'newToDo' is NOT an empty strinf
     if (newToDo !== '') {
-      // Use the SPREAD OPERATOR to create a shallow copy of 'toDoList' and append it with 'newToDo'
-      // Call the 'setToDoList' function to re-render the component
       setToDoList([...toDoList, newToDo]);
     }
   }
 
-  // Function that takes an index value as a parameter
   const handleDelete = (index) => {
-    // Call the setToDoList function and remove the target listItem from the 'toDoList' array
-    // using the filter() method. The callback function compares the index of each element (i) 
-    // with the index parameter passed in.
     setToDoList(toDoList.filter((item, i) => i !== index));
   }
 
-  // Return the JSX for the App component
   return (
         <main className="App">
         <div className="App-logo-container">
         <img src={require("./soc-logo.png")} className="App-logo" alt="logo" />
         </div>      
         <h1>Welcome to our ToDo App!</h1>
-        {/* Pass the 'handleAdd' function as a prop to the ToDoInput component */}
-        <ToDoInput handleAdd={handleAdd}/>
-        {/* Pass the 'toDoList' state variable and the 'handleDelete' function as props to the ToDoList component */}
-        <ToDoList toDoList={toDoList} inputValue={ToDoInput.inputValue} handleDelete={handleDelete}/>
+        <ToDoInput inputValue={inputValue} handleInputChange={handleInputChange} onClick={handleAddClick}/>
+        <ToDoList toDoList={toDoList} inputValue={inputValue} handleDelete={handleDelete}/>
         
         <div className="fillTheScreen"></div>
         </main>
@@ -48,11 +46,3 @@ function App() {
 }
 
 export default App;
-
-
-
-
-
-/* <header className="App-header">
- <img src={logo} className="App-logo" alt="logo" />
-</header> */
--- my-app/src/components/Input/index.js
+++ my-app/src/components/Input/index.js
@@ -1,33 +1,13 @@
-// Import any neccessary functions and components
-import React, { useState } from 'react';
-
 // ToDoInput function that takes a 'props' parameter
 export default function ToDoInput(props) {
 
-  // Declare a state variable called 'inputValue' and a function to update it's state called 'setInputValue'
-  const [inputValue, setInputValue] = useState('');
-  
-  // Function that takes an event as a parameter
-  const handleInputChange = (e) => {
-    // Call the 'setInputValue' function and update the state of 'inputValue' using the value of the input element
-    setInputValue(e.target.value);
-  }
-
-  // Function to handle the click event of the 'Add To List' button
-  const handleAddClick = () => {
-    // Call the 'handleAdd' function from the 'props' parameter and pass in the 'inputValue' state variable
-    props.handleAdd(inputValue);
-    // Update the state of 'inputValue' to an empty string
-    setInputValue('');
-    // console.log(inputValue);
-  }
-
+  const { inputValue, handleInputChange, onClick } = props;
   // Return the JSX for the ToDoInput component
   // Remember to enclose your JSX in a single parent element
   return  <div>
             {/* Call the 'handleInputChange' function when the input element changes */}
             <input className="textInput" type="text" placeholder="Enter a ToDo" value={inputValue} onChange={handleInputChange}/>
             {/* Call the 'handleAddClick' function when the 'Add To List' button is clicked */}
-            <button className="addButton" onClick={handleAddClick}>Add To List</button>
+            <button className="addButton" onClick={onClick}>Add To List</button>
           </div>
 }
